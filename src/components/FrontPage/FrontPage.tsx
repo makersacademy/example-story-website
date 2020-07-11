6e29ee286e5942853bd6442757bb7722e8c0d9d4
tommyrharper <tommyrharper@gmail.com>
2020-07-11 13:51:26 +0100

    Add image to FrontPage.tsx

--- src/components/FrontPage/FrontPage.tsx
+++ src/components/FrontPage/FrontPage.tsx
@@ -1,15 +1,26 @@
 import React from "react"
+import { Image } from "../../components/image"
+import "bootstrap/dist/css/bootstrap.css"
 
 import "./FrontPage.css"
 
 const FrontPage = () => {
-
-
   return (
     <>
       <div id="book">
-        <div id="spine">
-
+        <div className="row-2">
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <div id="spine"></div>
+              </div>
+              <div className="col-md p-5">
+                <Image />
+              </div>
+              
+              <div className="col"></div>
+            </div>
+          </div>
         </div>
       </div>
     </>
