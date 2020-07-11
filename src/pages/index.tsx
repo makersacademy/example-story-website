0667e92d2543d64ec3fe29b46634b7cbab7166fb
tommyrharper <tommyrharper@gmail.com>
2020-07-11 13:07:32 +0100

    Add FrontPage component to index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
+import { FrontPage } from '../components/FrontPage/FrontPage'
 
 import { Image, Layout, SEO } from "../components"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <FrontPage/>
+    {/* <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
   </Layout>
 )
 
