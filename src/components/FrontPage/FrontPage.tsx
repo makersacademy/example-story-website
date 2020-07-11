0b035e8c7ca23ef98a4265b374746cccf3e45a46
tommyrharper <tommyrharper@gmail.com>
2020-07-11 13:07:57 +0100

    Create FrontPage component

---
+++ src/components/FrontPage/FrontPage.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+import "./FrontPage.css"
+
+const FrontPage = () => {
+
+
+  return (
+    <>
+      <div id="book">
+        <div id="spine">
+
+        </div>
+        Hello
+      </div>
+    </>
+  )
+}
+
+export { FrontPage }
