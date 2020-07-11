66bf5d6fe32d531c155deda3a01bdbaa42591682
Ben Furber <ben@fu.rber.io>
2020-07-11 11:41:10 +0100

    ✨ Add module style to component exports

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import { Header } from "../components"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -48,4 +48,4 @@ Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
-export default Layout
+export { Layout }
