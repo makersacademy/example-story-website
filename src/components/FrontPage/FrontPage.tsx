3d62292d30d637994dffb724f4dc530f5b70a617
tommyrharper <tommyrharper@gmail.com>
2020-07-17 12:37:58 +0100

    Add in title to front page

--- src/components/FrontPage/FrontPage.tsx
+++ src/components/FrontPage/FrontPage.tsx
@@ -9,28 +9,11 @@ const FrontPage = () => {
     <>
       <div  className="container">
           <div className="row">
-            {/* <div className="col-3">1-3 of 12</div> */}
             <div className="col-6 offset-3"><Image/></div>
-            {/* <div className="col-5">5 of 12</div> */}
-            {/* <div className="col">6 of 12</div>
-            <div className="col">7 of 12</div> */}
-            {/* <div className="col">8 of 12</div> */}
-            {/* <div className="col">9 of 12</div> */}
-            {/* <div className="col-3">9-12 of 12</div> */}
-          </div>
+          </div> 
+          {/*Second Row*/}
           <div className="row">
-            <div className="col">1 of 12</div>
-            <div className="col">2 of 12</div>
-            <div className="col-8">3 of 12</div>
-            {/* <div className="col">4 of 12</div>
-            <div className="col">5 of 12</div>
-            <div className="col">6 of 12</div>
-            <div className="col">7 of 12</div>
-            <div className="col">8 of 12</div>
-            <div className="col">9 of 12</div>
-            <div className="col">10 of 12</div> */}
-            <div className="col">11 of 12</div>
-            <div className="col">12 of 12</div>
+            <div id="title" className="col-8 offset-2">asdfas</div>
           </div>
           <div className="row">
           <div className="col">1 of 12</div>
