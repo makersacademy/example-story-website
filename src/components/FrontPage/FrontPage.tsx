c3da01ca51f0f8b67aa5235897e816f840a0ce63
tommyrharper <tommyrharper@gmail.com>
2020-07-17 15:58:31 +0100

    Add grid within a grid to FrontPage

--- src/components/FrontPage/FrontPage.tsx
+++ src/components/FrontPage/FrontPage.tsx
@@ -7,29 +7,40 @@ import "./FrontPage.css"
 const FrontPage = () => {
   return (
     <>
-      <div  className="container">
-      <div id="spine"></div>
-          <div className="row">
-            <div className="col-6 offset-3 pt-5"><Image/></div>
-          </div> 
-          {/*Second Row*/}
-          <div className="row">
-            <div id="title" className="col-8 offset-2 text-center">
-              <h3 className="pt-1">
-                I'm Joe Blogs
-              <br></br>
-              I'm a Developer
-              </h3>
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-10 offset-1 p-0">
+            <div id="book" className="container">
+              <div id="spine"></div>
+              <div className="row">
+                <div className="col-6 offset-3 pt-5">
+                  <Image />
+                </div>
+              </div>
+              {/*Second Row*/}
+              <div className="row">
+                <div id="title" className="col-8 offset-2 text-center">
+                  <h3 className="pt-1">
+                    I'm Joe Blogs
+                    <br></br>
+                    I'm a Developer
+                  </h3>
+                </div>
+              </div>
+              {/*Third Row*/}
+              <div className="row">
+                <div className="col-8 offset-2 text-center px-5 pt-4">
+                  <h2>
+                    I’m looking for My first job as a full-stack developer
+                  </h2>
+                </div>
+              </div>
             </div>
           </div>
-        {/*Third Row*/}
-          <div className="row">
-          <div  className="col-8 offset-2 text-center px-5 pt-4"  >
-            <h2>I’m looking for My first job as a full-stack developer</h2>
-        
+          <div className="col-1 p-0" style={{ color: "red" }}>
+            <div id="triangle">asdfasdf</div>
           </div>
-          </div>
-          
+        </div>
       </div>
     </>
   )
