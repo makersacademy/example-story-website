a543f3687436d0fee8349ac18789d28cec7ae2d3
tommyrharper <tommyrharper@gmail.com>
2020-07-17 12:26:08 +0100

    Use offset in bootstrap grid

--- src/components/FrontPage/FrontPage.tsx
+++ src/components/FrontPage/FrontPage.tsx
@@ -9,15 +9,14 @@ const FrontPage = () => {
     <>
       <div  className="container">
           <div className="row">
-            <div className="col-3">1-3 of 12</div>
-            <div className="col-6"><Image/></div>
+            {/* <div className="col-3">1-3 of 12</div> */}
+            <div className="col-6 offset-3"><Image/></div>
             {/* <div className="col-5">5 of 12</div> */}
             {/* <div className="col">6 of 12</div>
             <div className="col">7 of 12</div> */}
             {/* <div className="col">8 of 12</div> */}
             {/* <div className="col">9 of 12</div> */}
-
-            <div className="col-3">9-12 of 12</div>
+            {/* <div className="col-3">9-12 of 12</div> */}
           </div>
           <div className="row">
             <div className="col">1 of 12</div>
