9848656dc54c3718b04a264a470d769ce7a5dbf8
tommyrharper <tommyrharper@gmail.com>
2020-07-17 12:23:03 +0100

    Use Bootstrap Grid system properly

--- src/components/FrontPage/FrontPage.tsx
+++ src/components/FrontPage/FrontPage.tsx
@@ -7,18 +7,87 @@ import "./FrontPage.css"
 const FrontPage = () => {
   return (
     <>
-      <div id="book">
-          <div className="container">
-            <div className="row">
-              <div className="col">
-                <div id="spine"></div>
-              </div>
-              <div className="col-md p-5">
-                <Image />
-              <div id="title"></div>
-              </div>
-              <div className="col"></div>
-            </div>
+      <div  className="container">
+          <div className="row">
+            <div className="col-3">1-3 of 12</div>
+            <div className="col-6"><Image/></div>
+            {/* <div className="col-5">5 of 12</div> */}
+            {/* <div className="col">6 of 12</div>
+            <div className="col">7 of 12</div> */}
+            {/* <div className="col">8 of 12</div> */}
+            {/* <div className="col">9 of 12</div> */}
+
+            <div className="col-3">9-12 of 12</div>
+          </div>
+          <div className="row">
+            <div className="col">1 of 12</div>
+            <div className="col">2 of 12</div>
+            <div className="col-8">3 of 12</div>
+            {/* <div className="col">4 of 12</div>
+            <div className="col">5 of 12</div>
+            <div className="col">6 of 12</div>
+            <div className="col">7 of 12</div>
+            <div className="col">8 of 12</div>
+            <div className="col">9 of 12</div>
+            <div className="col">10 of 12</div> */}
+            <div className="col">11 of 12</div>
+            <div className="col">12 of 12</div>
+          </div>
+          <div className="row">
+          <div className="col">1 of 12</div>
+            <div className="col">2 of 12</div>
+            <div className="col">3 of 12</div>
+            <div className="col">4 of 12</div>
+            <div className="col">5 of 12</div>
+            <div className="col">6 of 12</div>
+            <div className="col">7 of 12</div>
+            <div className="col">8 of 12</div>
+            <div className="col">9 of 12</div>
+            <div className="col">10 of 12</div>
+            <div className="col">11 of 12</div>
+            <div className="col">12 of 12</div>
+          </div>
+          <div className="row">
+          <div className="col">1 of 12</div>
+            <div className="col">2 of 12</div>
+            <div className="col">3 of 12</div>
+            <div className="col">4 of 12</div>
+            <div className="col">5 of 12</div>
+            <div className="col">6 of 12</div>
+            <div className="col">7 of 12</div>
+            <div className="col">8 of 12</div>
+            <div className="col">9 of 12</div>
+            <div className="col">10 of 12</div>
+            <div className="col">11 of 12</div>
+            <div className="col">12 of 12</div>
+          </div>
+          <div className="row">
+            <div className="col">1 of 12</div>
+            <div className="col">2 of 12</div>
+            <div className="col">3 of 12</div>
+            <div className="col">4 of 12</div>
+            <div className="col">5 of 12</div>
+            <div className="col">6 of 12</div>
+            <div className="col">7 of 12</div>
+            <div className="col">8 of 12</div>
+            <div className="col">9 of 12</div>
+            <div className="col">10 of 12</div>
+            <div className="col">11 of 12</div>
+            <div className="col">12 of 12</div>
+          </div>
+          <div className="row">
+            <div className="col">1 of 12</div>
+            <div className="col">2 of 12</div>
+            <div className="col">3 of 12</div>
+            <div className="col">4 of 12</div>
+            <div className="col">5 of 12</div>
+            <div className="col">6 of 12</div>
+            <div className="col">7 of 12</div>
+            <div className="col">8 of 12</div>
+            <div className="col">9 of 12</div>
+            <div className="col">10 of 12</div>
+            <div className="col">11 of 12</div>
+            <div className="col">12 of 12</div>
           </div>
       </div>
     </>
@@ -26,3 +95,17 @@ const FrontPage = () => {
 }
 
 export { FrontPage }
+
+
+ {/* <div className="container">
+<div className="row">
+  <div className="col">
+    <div id="spine"></div>
+  </div>
+  <div className="col-md p-5">
+    <Image />
+  <div id="title"></div>
+  </div>
+  <div className="col"></div>
+</div>
+</div> */}
