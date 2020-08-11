088fbad5403f59fda20e011b38677e9ff11ca61f
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-08-11 12:20:19 +0100

    add chapter list and bootstrap to navBar

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,10 +1,22 @@
 import React from "react"
-
+import "bootstrap/dist/css/bootstrap.css"
+import "./navBar.scss"
+import { NavBarLabels } from "./index"
 
 export const NavBar = () => {
+  const listItems = NavBarLabels.chapters.map(chapter => (
+    <a className="nav-link" href="#">
+      {chapter}
+    </a>
+  ))
   return (
-
-   <h3>NAV BAR!</h3>
-
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark container">
+        <div>
+          <ul className="ml-auto navbar-nav">{listItems}</ul>
+          <div className="rectangle"></div>
+        </div>
+      </nav>
+    </div>
   )
 }
