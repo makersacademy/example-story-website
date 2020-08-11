2983185b83b726b8da49e0333873b67a80712591
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-08-11 11:07:51 +0100

    deleted boiler plate and added NavBar

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -10,39 +10,27 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import { Header } from "../../components"
+import { NavBar } from "../navBar"
 import "./index.scss"
 
-  const Layout = ({ children }) => {
-    const data = useStaticQuery(graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
         }
       }
-    `)
+    }
+  `)
 
-    return (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0 1.0875rem 1.45rem`,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )
-  }
+  return (
+    <div>
+      <NavBar />
+      <main>{children}</main>
+    </div>
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
