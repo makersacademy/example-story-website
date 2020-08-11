1913efac7e8098ca9678952d267984967fa8c3cc
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-08-11 11:08:26 +0100

    delete boiler plate

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,19 +10,7 @@ const Header = (props: Props) => {
   const { siteTitle } = props
 
   return (
-    <header
-      style={{
-        background: `rebeccapurple`,
-        marginBottom: `1.45rem`,
-      }}
-    >
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `1.45rem 1.0875rem`,
-        }}
-      >
+
         <h1 style={{ margin: 0 }}>
           <Link
             to="/"
@@ -34,8 +22,8 @@ const Header = (props: Props) => {
             {siteTitle}
           </Link>
         </h1>
-      </div>
-    </header>
+
+
   )
 }
 
