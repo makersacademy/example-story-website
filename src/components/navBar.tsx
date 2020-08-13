ff81659be3a14302983d70f90a16f5fce393f78f
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-08-13 11:50:09 +0100

    Add primary color and font-family sass variables to navBar

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -6,14 +6,14 @@ import { NavBarLabels } from "./index"
 export const NavBar = () => {
   const listItems = NavBarLabels.chapters.map(chapter => (
     <a className="nav-link" href="#">
-      {chapter}
+      <p className="navText">{chapter}</p>
     </a>
   ))
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark container">
+      <nav className="navbar navbar-expand-lg navbar-dark container py-5 ">
         <div>
-          <ul className="ml-auto navbar-nav">{listItems}</ul>
+          <ul className=" ml-auto navbar-nav">{listItems}</ul>
           <div className="rectangle"></div>
         </div>
       </nav>
