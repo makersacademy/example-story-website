d1af9ddc9cc95540022742b1b4cddee935fb3767
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-08-18 15:43:36 +0100

    add left and right pages to ChapterPages component

--- src/components/chapterPages/chapterPages.tsx
+++ src/components/chapterPages/chapterPages.tsx
@@ -1,14 +1,16 @@
 import React from "react"
 import "./chapterPages.scss"
+import "bootstrap/dist/css/bootstrap.css"
 
 export const ChapterPages = () => {
   return (
     <div className="container">
-      <div className="row align-items-center">
-        <div className="col-10 offset-1 p-0">
-          <div id="book" className="container">
-            <div id="spine"></div>
-          </div>
+      <div className="row">
+        <div className="col-sm">
+          <div id="leftPage" className="container"></div>
+        </div>
+        <div className="col-sm">
+          <div id="rightPage" className="container"></div>
         </div>
       </div>
     </div>
