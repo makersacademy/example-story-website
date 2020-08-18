d5f3b5438b63fe8025f881dda7e1d9d1b1294371
Gina Frankel <g.frankel22@gmail.com>
2020-08-18 16:55:59 +0100

    Extract leftPage + rightPage from chapterPages

--- src/components/chapterPages/chapterPages.tsx
+++ src/components/chapterPages/chapterPages.tsx
@@ -1,27 +1,18 @@
 import React from "react"
 import "./chapterPages.scss"
 import "bootstrap/dist/css/bootstrap.css"
-
+import { RightPage } from "../index"
+import { LeftPage } from "../index"
 export const ChapterPages = () => {
   return (
     <div className="container">
       <div className="row">
         <div className="col-sm px-0">
-          <div id="leftPage"></div>
+          <LeftPage />
         </div>
+
         <div className="col-sm px-0">
-          <div id="rightPage">
-            <div>
-              <div className="row">
-                <div className="col-lg-5 px-0">
-                  <div id="greyBox"></div>
-                </div>
-                <div className="col-lg-4 px-0">
-                  <div id="whiteBox"></div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <RightPage />
         </div>
       </div>
     </div>
---
+++ src/components/chapterPages/leftPage.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+import "bootstrap/dist/css/bootstrap.css"
+import "./chapterPages.scss"
+
+export const LeftPage = () => {
+  return <div id="leftPage"></div>
+}
