1bc7389627cd3f3910682fef4828f2250f84d4ba
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-08-18 15:17:38 +0100

    capitalise ChapterPages

--- src/components/chapterPages/chapterPages.tsx
+++ src/components/chapterPages/chapterPages.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import './chapterPages.scss'; 
+import React from "react"
+import "./chapterPages.scss"
 
-export const chapterPages = () => {
-    return (
-      <div className="container">
-        <div className="row align-items-center">
-          <div className="col-10 offset-1 p-0">
-            <div id="book" className="container">
-              <div id="spine"></div>
-           
-            </div>
+export const ChapterPages = () => {
+  return (
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col-10 offset-1 p-0">
+          <div id="book" className="container">
+            <div id="spine"></div>
           </div>
         </div>
       </div>
-    )
-  }
-  
+    </div>
+  )
+}
