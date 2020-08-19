55d7c5a81e32d13f99a1fa8629de0ca18033b8dc
Gina Frankel <g.frankel22@gmail.com>
2020-08-19 16:39:08 +0100

    Add props to test

--- src/components/layout/layout.spec.tsx
+++ src/components/layout/layout.spec.tsx
@@ -3,6 +3,10 @@ import { shallow } from "enzyme"
 import { Layout } from "./index"
 
 it("renders a page correctly", () => {
-  let wrapper = shallow(<Layout />)
+  let wrapper = shallow(
+    <Layout title="hi">
+      <div></div>
+    </Layout>
+  )
   expect(wrapper).toHaveLength(1)
 })
