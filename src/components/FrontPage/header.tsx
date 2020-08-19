ebf0e40c733889b2c18fd44662ef43e96b938955
Gina Frankel <g.frankel22@gmail.com>
2020-08-19 17:11:50 +0100

    Change name

--- src/components/FrontPage/header.tsx
+++ src/components/FrontPage/header.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Image } from "../../components/image"
+import { Image } from "../image"
 import "bootstrap/dist/css/bootstrap.css"
 import { FrontPageLabels } from "../index"
 
