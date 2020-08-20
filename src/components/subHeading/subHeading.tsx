577ee73a28564454cb11108ba69272a7c5ee5d79
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-08-20 16:49:54 +0100

    Add Heading scss

--- src/components/subHeading/subHeading.tsx
+++ src/components/subHeading/subHeading.tsx
@@ -3,14 +3,14 @@ import { Image } from "../image"
 import "bootstrap/dist/css/bootstrap.css"
 import { BookFrontCoverLabels } from "../index"
 
-import "../bookFrontCover/bookFrontCover.scss"
+import "./subHeading.scss"
 
 export const SubHeading = () => {
   return (
     <div>
       <div className="row">
         <div className="col-8 offset-2 text-center px-5 pt-4">
-          <h2>{BookFrontCoverLabels.intro3}</h2>
+          <h2 className="subHeading">{BookFrontCoverLabels.intro3}</h2>
         </div>
       </div>
     </div>
