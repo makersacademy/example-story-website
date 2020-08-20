96eb41cefe00bb227b968f18e36d6356e6ebdd2f
Gina Frankel <g.frankel22@gmail.com>
2020-08-20 15:28:04 +0100

    Import bookFrontCover scss

--- src/components/heading/heading.tsx
+++ src/components/heading/heading.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 import "bootstrap/dist/css/bootstrap.css"
 import { BookFrontCoverLabels } from "../index"
-
+import "../bookFrontCover/bookFrontCover.scss"
 // import "./FrontPage.scss"
 
 export const Heading = () => {
@@ -10,7 +10,7 @@ export const Heading = () => {
     <div>
       <div className="row">
         <div id="title" className="col-8 offset-2 text-center">
-          <h3 className="pt-1">
+          <h3 className="pt-1 heading">
             {BookFrontCoverLabels.intro1}
             <br></br>
             {BookFrontCoverLabels.intro2}
