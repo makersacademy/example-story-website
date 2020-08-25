ab8ccfcb2f0af2b38583414fac246e9e0a5755cb
Gina Frankel <g.frankel22@gmail.com>
2020-08-25 15:31:59 +0100

    Test for greeting in Heading component

--- src/components/heading/heading.spec.tsx
+++ src/components/heading/heading.spec.tsx
@@ -6,3 +6,10 @@ it("renders a page correctly", () => {
   let wrapper = shallow(<Heading greeting="hi" role="developer" />)
   expect(wrapper).toHaveLength(1)
 })
+
+it("renders a greeting", () => {
+  let wrapper = shallow(
+    <Heading greeting="Hi, I am a test person" role="developer" />
+  )
+  expect(wrapper.text()).toContain("Hi, I am a test person")
+})
