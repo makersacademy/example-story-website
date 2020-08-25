744c1258caf2461f4186e39e354f5b89fec2ca9f
Gina Frankel <g.frankel22@gmail.com>
2020-08-25 15:24:51 +0100

    Fix test by deleting div

--- src/components/subHeading/subHeading.spec.tsx
+++ src/components/subHeading/subHeading.spec.tsx
@@ -3,10 +3,6 @@ import { shallow } from "enzyme"
 import { SubHeading } from "../index"
 
 it("renders a page correctly", () => {
-  let wrapper = shallow(
-    <SubHeading aim="">
-      <div></div>
-    </SubHeading>
-  )
+  let wrapper = shallow(<SubHeading careerAim="Super developer"></SubHeading>)
   expect(wrapper).toHaveLength(1)
 })
