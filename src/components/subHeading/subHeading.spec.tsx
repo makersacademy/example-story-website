c6c85a06b7f74b7952cb2d1b775cb5a55a37d3ca
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-08-25 16:08:39 +0100

    Test for rendering of careerAim

--- src/components/subHeading/subHeading.spec.tsx
+++ src/components/subHeading/subHeading.spec.tsx
@@ -6,3 +6,10 @@ it("renders a page correctly", () => {
   let wrapper = shallow(<SubHeading careerAim="Super developer"></SubHeading>)
   expect(wrapper).toHaveLength(1)
 })
+
+it("renders a careerAim", () => {
+  let wrapper = shallow(
+    <SubHeading careerAim="I would like to be a full-stack tester" />
+  )
+  expect(wrapper.text()).toContain("I would like to be a full-stack tester")
+})
