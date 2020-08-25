216c6f6faf40d01c7cc222272ee4bb39afea79e5
Gina Frankel <g.frankel22@gmail.com>
2020-08-25 15:41:16 +0100

    Test for rendering of jobTitle

--- src/components/heading/heading.spec.tsx
+++ src/components/heading/heading.spec.tsx
@@ -3,13 +3,20 @@ import { shallow } from "enzyme"
 import { Heading } from "../index"
 
 it("renders a page correctly", () => {
-  let wrapper = shallow(<Heading greeting="hi" role="developer" />)
+  let wrapper = shallow(<Heading greeting="hi" jobTitle="developer" />)
   expect(wrapper).toHaveLength(1)
 })
 
 it("renders a greeting", () => {
   let wrapper = shallow(
-    <Heading greeting="Hi, I am a test person" role="developer" />
+    <Heading greeting="Hi, I am a test person" jobTitle="developer" />
   )
   expect(wrapper.text()).toContain("Hi, I am a test person")
+})
+
+it("renders a jobTitle", () => {
+  let wrapper = shallow(
+    <Heading greeting="Hi, I am a test person" jobTitle="I am a tester" />
+  )
+  expect(wrapper.text()).toContain("I am a tester")
 })
