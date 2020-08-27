83f79ae72f349e2d25d161f72fe533ef196c3af0
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-08-27 16:05:36 +0100

    Export index markdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,40 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import { BookFrontCover } from "../components/index"
 
 import { Layout, SEO } from "../components"
 
-const IndexPage = () => (
+const md = data => {
+  const { frontmatter, html } = data.data.allMarkdownRemark.edges[0].node
+  return (
+    <div>
+      <h1>{frontmatter.title}</h1>
+      <p>{frontmatter.date}</p>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </div>
+  )
+}
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <BookFrontCover />
+    <BookFrontCover data={data.allMarkdownRemark.edges[0].node.frontmatter} />
   </Layout>
 )
+export const query = graphql`
+  query {
+    allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/index/" } }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            date(formatString: "DD MMMM YYYY")
+          }
+          html
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
