60ee25bad44a08e9a53ad535ea0d751f91499843
Gina Frankel <g.frankel22@gmail.com>
2020-08-27 17:40:25 +0100

    Delete unused bookFrontCover object

--- src/labels.tsx
+++ src/labels.tsx
@@ -1,9 +1,3 @@
-export const bookFrontCover = {
-  greeting: "Hi I'm Joe Bloggs",
-  jobTitle: "I'm a Developer",
-  careerAim: "I'm looking for my first job as a Full-Stack Developer",
-}
-
 export const navBar = {
   chapters: [
     "Home",
