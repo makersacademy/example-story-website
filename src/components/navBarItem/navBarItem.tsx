b14a0cad21659b1666eea5c2811fe9439c6c4736
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-02 16:36:15 +0100

    Update to create links based on props

--- src/components/navBarItem/navBarItem.tsx
+++ src/components/navBarItem/navBarItem.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react"
 import "bootstrap/dist/css/bootstrap.css"
 
-export const NavBarItem = (props) => {
-    return (
-        <a className="nav-link" href="/chapter1">
-        <p className="navText">{props.chapter}</p>
-      </a>
-    )
+export const NavBarItem = props => {
+  return (
+    <a className="nav-link" href={props.chapter}>
+      <p className="navText">{props.chapter}</p>
+    </a>
+  )
 }
