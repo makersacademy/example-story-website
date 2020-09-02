73d50d07a38fcbb1e3c6a97a8dd2441314151a3e
Gina Frankel <g.frankel22@gmail.com>
2020-09-02 18:24:15 +0100

    :art: Move contents of ChapterPages component into DoublePage component

--- src/components/doublePage/doublePage.tsx
+++ src/components/doublePage/doublePage.tsx
@@ -1,15 +1,35 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.css"
 
-import { ChapterPages, BookSection, Line } from "../index"
+import { ChapterPages, BookSection } from "../index"
 import "./doublePage.scss"
 
 export const DoublePage = () => {
   return (
-    <div className="container">
+    <div className="container " id="double-page">
+      {/* row 1 */}
       <div className="row">
-        <div className="col-xl-12">
-          <ChapterPages />
+        <div className=" col-md-6 book-mid-line  ">
+          <h2 className="heading ">Chapter 2: Test chapter</h2>
+        </div>
+      </div>
+
+      {/* row 2 */}
+      <div className="row">
+        <div className=" col-md-6 ">
+          <BookSection line={true} />
+        </div>
+        <div className=" col-md-6 ">
+          <BookSection />
+        </div>
+      </div>
+      {/* row 3 */}
+      <div className="row">
+        <div className=" col-md-6 ">
+          <BookSection line={true} />
+        </div>
+        <div className=" col-md-6 ">
+          <BookSection />
         </div>
       </div>
     </div>
