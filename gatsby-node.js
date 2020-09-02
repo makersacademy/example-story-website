8616e3ee0740e687c8a1da60e62998bff40bbbb6
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-02 16:35:43 +0100

    Update to create pages from markdown data

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,25 @@
-exports.createPages = async ({ actions, graphql, reporter }) => {
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-
   const result = await graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
+    query {
+      allMarkdownRemark {
         edges {
           node {
-            frontmatter {
-
+            fields {
+              slug
             }
           }
         }
@@ -18,8 +27,38 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     }
   `)
 
-  if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
-  }
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/chapter.js`),
+      context: {
+        slug: node.fields.slug,
+      },
+    })
+  })
 }
+// exports.createPages = async ({ actions, graphql, reporter }) => {
+//   const { createPage } = actions
+//
+//   const result = await graphql(`
+//     {
+//       allMarkdownRemark(
+//         sort: { order: DESC, fields: [frontmatter___date] }
+//         limit: 1000
+//       ) {
+//         edges {
+//           node {
+//             frontmatter {
+//
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
+//
+//   if (result.errors) {
+//     reporter.panicOnBuild(`Error while running GraphQL query.`)
+//     return
+//   }
+// }
