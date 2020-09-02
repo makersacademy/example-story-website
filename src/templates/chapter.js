312d38271c3ce510ffbc2d10ddb8fb2ffb8cda46
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-02 17:34:51 +0100

    Refactor to display title outside of DoublePage

--- src/templates/chapter.js
+++ src/templates/chapter.js
@@ -8,12 +8,11 @@ export default function NewChapter({ data }) {
   return (
     <div>
       <Layout>
-        <DoublePage>
-          <div>
-            <h1>{post.frontmatter.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: post.html }} />
-          </div>
-        </DoublePage>
+        <DoublePage />
+        <div>
+          <h1>{post.frontmatter.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        </div>
       </Layout>
     </div>
   )
