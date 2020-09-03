3d45330eb3b8925c330e486e1db8312912be1500
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-03 17:12:44 +0100

    Remove comments

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,28 +37,3 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 }
-// exports.createPages = async ({ actions, graphql, reporter }) => {
-//   const { createPage } = actions
-//
-//   const result = await graphql(`
-//     {
-//       allMarkdownRemark(
-//         sort: { order: DESC, fields: [frontmatter___date] }
-//         limit: 1000
-//       ) {
-//         edges {
-//           node {
-//             frontmatter {
-//
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `)
-//
-//   if (result.errors) {
-//     reporter.panicOnBuild(`Error while running GraphQL query.`)
-//     return
-//   }
-// }
