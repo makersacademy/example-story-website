649e595fcd541d61891c95b099886203e459fdf8
Gina Frankel <g.frankel22@gmail.com>
2020-09-03 17:20:11 +0100

    :fire: Remove code to make line appear in middle of book

--- src/components/bookSection/bookSection.tsx
+++ src/components/bookSection/bookSection.tsx
@@ -3,15 +3,9 @@ import "bootstrap/dist/css/bootstrap.css"
 import "./bookSection.scss"
 
 export const BookSection = props => {
-  let customStyleRule
-  if (props.line) {
-    customStyleRule = { "border-right": "1px solid black" }
-  }
   return (
-    <div style={customStyleRule}>
-      <div className="  px-3 py-2">
-        <div className="box"></div>
-      </div>
+    <div className="  px-3 py-2">
+      <div className="box"></div>
     </div>
   )
 }
