b3bdb2d15c330466eab68e9029876e9a6da57ec0
Gina Frankel <g.frankel22@gmail.com>
2020-09-04 16:23:15 +0100

    :recycle: Extract layout styling from DoublePage to BookSection

--- src/components/bookSection/bookSection.tsx
+++ src/components/bookSection/bookSection.tsx
@@ -2,10 +2,17 @@ import React from "react"
 import "bootstrap/dist/css/bootstrap.css"
 import "./bookSection.scss"
 
-export const BookSection = () => {
+export const BookSection = props => {
+  let style
+  if (props.midLine) {
+    console.log("true")
+    style = "book-mid-line"
+  }
   return (
-    <div className="px-3 py-2">
-      <div className="box"></div>
+    <div className={`col-md-6 col-lg-6 ${style}`}>
+      <div className="px-3 py-2">
+        <div className="box"></div>
+      </div>
     </div>
   )
 }
