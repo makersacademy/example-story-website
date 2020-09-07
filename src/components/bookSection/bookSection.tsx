32a4bc2bb7aca89d13da4ae3a9eed43999e0a657
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-07 15:31:47 +0100

    Deconstruct props and define Props types

--- src/components/bookSection/bookSection.tsx
+++ src/components/bookSection/bookSection.tsx
@@ -2,16 +2,21 @@ import React from "react"
 import "bootstrap/dist/css/bootstrap.css"
 import "./bookSection.scss"
 
-export const BookSection = props => {
+interface Props {
+  midLine: boolean
+  section1: string
+}
+
+export const BookSection = (props: Props) => {
+  const { midLine, section1 } = props
   let style
-  if (props.midLine) {
-    console.log("true")
+  if (midLine) {
     style = "book-mid-line"
   }
   return (
     <div className={`col-md-6 col-lg-6 ${style}`}>
       <div className="px-3 py-2">
-        <div className="box">{props.section1}</div>
+        <div className="box">{section1}</div>
       </div>
     </div>
   )
