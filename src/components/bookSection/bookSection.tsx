26a8a5c6a4bf1ecbc4d06f20b50d50a9ad65ad25
James Forster <jamesalexforster@gmail.com>
2020-09-16 15:43:10 +0100

    updated styling for bookSection

--- src/components/bookSection/bookSection.tsx
+++ src/components/bookSection/bookSection.tsx
@@ -23,8 +23,12 @@ export const BookSection = (props: Props) => {
     <div className={`col-md-6 col-lg-6 ${style}`}>
       <div className="px-3 py-2">
         <div className="box">
-          {title}
-          <p>{subTitle}</p>
+          <div className="boxTitle"> 
+            {title}
+          </div>
+          <div className="boxBody">
+            {subTitle}
+          </div>
         </div>
       </div>
     </div>
