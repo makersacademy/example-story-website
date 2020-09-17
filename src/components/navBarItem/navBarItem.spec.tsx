36e36f229143418efc004141de813509123f56d3
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-17 23:54:43 +0100

    Update test to use new props

--- src/components/navBarItem/navBarItem.spec.tsx
+++ src/components/navBarItem/navBarItem.spec.tsx
@@ -3,6 +3,8 @@ import { shallow } from "enzyme"
 import { NavBarItem } from "../index"
 
 it("renders a chapter", () => {
-  let wrapper = shallow(<NavBarItem chapter="Learning" />)
+  let wrapper = shallow(
+    <NavBarItem chapterTitle="Learning" chapterLink="/learning/" />
+  )
   expect(wrapper.text()).toContain("Learning")
 })
