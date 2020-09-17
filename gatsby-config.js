cd6412066bd7d1b9b27fc6d30a21f58af3608059
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-17 15:43:45 +0100

    Update siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Joe Bloggs - Software Developer`,
+    description: `Joe's Portfolio Site`,
+    author: `@joebloggs`,
   },
   plugins: [
     `gatsby-plugin-sass`,
