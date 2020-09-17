f1a6bdc09dc698eb91e3b90db5db8d07bbbe3671
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-18 00:26:58 +0100

    Fix gatsby import error

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.css"
-import { useStaticQuery, gatsby } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 import { NavBarItem } from "../index"
 import "./navBar.scss"
 
