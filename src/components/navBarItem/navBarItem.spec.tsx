cbda460387e47aa4cf9dc192c6147fc5df75460f
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-18 23:31:39 +0100

    Add describe block to enable test

--- src/components/navBarItem/navBarItem.spec.tsx
+++ src/components/navBarItem/navBarItem.spec.tsx
@@ -2,9 +2,11 @@ import React from "react"
 import { shallow } from "enzyme"
 import { NavBarItem } from "../index"
 
-it("renders a chapter", () => {
-  let wrapper = shallow(
-    <NavBarItem chapterTitle="Learning" chapterLink="/learning/" />
-  )
-  expect(wrapper.text()).toContain("Learning")
+describe("NavBarItem", () => {
+  it("renders a chapter", () => {
+    let wrapper = shallow(
+      <NavBarItem chapterTitle="Learning" chapterLink="/learning/" />
+    )
+    expect(wrapper.text()).toContain("Learning")
+  })
 })
