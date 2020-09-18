f34186c85635a7472e94dcff8a87a95e83b57a35
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-18 23:31:52 +0100

    Add describe block to enable test

--- src/components/subHeading/subHeading.spec.tsx
+++ src/components/subHeading/subHeading.spec.tsx
@@ -2,14 +2,16 @@ import React from "react"
 import { shallow } from "enzyme"
 import { SubHeading } from "../index"
 
-it("renders a page correctly", () => {
-  let wrapper = shallow(<SubHeading careerAim="Super developer"></SubHeading>)
-  expect(wrapper).toHaveLength(1)
-})
+describe("SubHeading", () => {
+  it("renders a page correctly", () => {
+    let wrapper = shallow(<SubHeading careerAim="Super developer"></SubHeading>)
+    expect(wrapper).toHaveLength(1)
+  })
 
-it("renders a careerAim", () => {
-  let wrapper = shallow(
-    <SubHeading careerAim="I would like to be a full-stack tester" />
-  )
-  expect(wrapper.text()).toContain("I would like to be a full-stack tester")
+  it("renders a careerAim", () => {
+    let wrapper = shallow(
+      <SubHeading careerAim="I would like to be a full-stack tester" />
+    )
+    expect(wrapper.text()).toContain("I would like to be a full-stack tester")
+  })
 })
