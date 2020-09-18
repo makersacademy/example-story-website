7ffcc91d627efe0e6c0d007e44c8739851d631ec
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-18 23:30:33 +0100

    Add describe block to enable test

--- src/components/layout/index.spec.tsx
+++ src/components/layout/index.spec.tsx
@@ -2,10 +2,9 @@ import React from "react"
 import { shallow } from "enzyme"
 import { Layout } from "./index"
 
-it("renders a page correctly", () => {
-  let wrapper = shallow(
-    <Layout >
-    </Layout>
-  )
-  expect(wrapper).toHaveLength(1)
+describe("Layout", () => {
+  it("renders a page correctly", () => {
+    let wrapper = shallow(<Layout></Layout>)
+    expect(wrapper).toHaveLength(1)
+  })
 })
