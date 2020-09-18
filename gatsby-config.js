c1bd6d53a7a46ac41e3a32bff95c8df4ddbf4745
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-18 23:23:34 +0100

    Add gatsby-plugin-testing to enable graphql testing

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-testing`,
     {
       resolve: `gatsby-source-filesystem`,
       options: { name: `images`, path: `${__dirname}/src/images` },
