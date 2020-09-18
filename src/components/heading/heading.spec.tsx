ccf4dd99e47b3ba2e6c0e41f9c45ee301870f2f3
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-18 23:28:07 +0100

    Add describe block to enable test

--- src/components/heading/heading.spec.tsx
+++ src/components/heading/heading.spec.tsx
@@ -2,21 +2,23 @@ import React from "react"
 import { shallow } from "enzyme"
 import { Heading } from "../index"
 
-it("renders a page correctly", () => {
-  let wrapper = shallow(<Heading greeting="hi" jobTitle="developer" />)
-  expect(wrapper).toHaveLength(1)
-})
+describe("Heading", () => {
+  it("renders a page correctly", () => {
+    let wrapper = shallow(<Heading greeting="hi" jobTitle="developer" />)
+    expect(wrapper).toHaveLength(1)
+  })
 
-it("renders a greeting", () => {
-  let wrapper = shallow(
-    <Heading greeting="Hi, I am a test person" jobTitle="developer" />
-  )
-  expect(wrapper.text()).toContain("Hi, I am a test person")
-})
+  it("renders a greeting", () => {
+    let wrapper = shallow(
+      <Heading greeting="Hi, I am a test person" jobTitle="developer" />
+    )
+    expect(wrapper.text()).toContain("Hi, I am a test person")
+  })
 
-it("renders a jobTitle", () => {
-  let wrapper = shallow(
-    <Heading greeting="Hi, I am a test person" jobTitle="I am a tester" />
-  )
-  expect(wrapper.text()).toContain("I am a tester")
+  it("renders a jobTitle", () => {
+    let wrapper = shallow(
+      <Heading greeting="Hi, I am a test person" jobTitle="I am a tester" />
+    )
+    expect(wrapper.text()).toContain("I am a tester")
+  })
 })
