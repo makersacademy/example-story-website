5ec0125aeb13d26b16b21980fd4885124213bce0
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-18 23:26:47 +0100

    Add describe block to enable test

--- src/components/bookFrontCover/bookFrontCover.spec.tsx
+++ src/components/bookFrontCover/bookFrontCover.spec.tsx
@@ -2,7 +2,15 @@ import React from "react"
 import { shallow } from "enzyme"
 import { BookFrontCover } from "../index"
 
-it("renders a page correctly", () => {
-  let wrapper = shallow(<BookFrontCover greeting="hello" jobTitle="developer" careerAim="data ops engineer" />)
-  expect(wrapper).toHaveLength(1)
+describe("BookFronCover", () => {
+  it("renders a page correctly", () => {
+    let wrapper = shallow(
+      <BookFrontCover
+        greeting="hello"
+        jobTitle="developer"
+        careerAim="data ops engineer"
+      />
+    )
+    expect(wrapper).toHaveLength(1)
+  })
 })
