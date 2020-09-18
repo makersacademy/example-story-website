c48b7b6da4a527d6c69fc54d1234516fb4ee087e
Gina Frankel <g.frankel22@gmail.com>
2020-09-18 12:34:04 +0100

    :recycle: Rename sass classes to describe function

--- src/components/bookSection/bookSection.tsx
+++ src/components/bookSection/bookSection.tsx
@@ -26,7 +26,7 @@ export const BookSection = (props: Props) => {
       <div className="px-3 py-2">
         <div className="box">
           <h2 className="box-title px-2 my-0 py-2 ">{title}</h2>
-          <h3 className="box-body px-2">{subTitle}</h3>
+          <h3 className="box-subTitle px-2">{subTitle}</h3>
           <p className="px-2 box-content">{content}</p>
         </div>
       </div>
