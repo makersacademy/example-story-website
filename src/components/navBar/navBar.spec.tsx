4a4178a58a1caf99af4e0916025214e82c249cef
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-18 23:31:06 +0100

    Add describe block to enable test

--- src/components/navBar/navBar.spec.tsx
+++ src/components/navBar/navBar.spec.tsx
@@ -2,7 +2,9 @@ import React from "react"
 import { shallow } from "enzyme"
 import { NavBar } from "../index"
 
-it("renders the NavBar correctly", () => {
-    let wrapper = shallow(<NavBar ></NavBar>)
+describe("NavBar", () => {
+  it("renders the NavBar correctly", () => {
+    let wrapper = shallow(<NavBar></NavBar>)
     expect(wrapper).toHaveLength(1)
+  })
 })
