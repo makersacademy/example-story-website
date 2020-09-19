82a316e52eeb8266f2eb97ead8401fd47b6ca8ce
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-19 14:47:25 +0100

    WIP

--- src/components/navBar/navBar.spec.tsx
+++ src/components/navBar/navBar.spec.tsx
@@ -1,10 +1,14 @@
 import React from "react"
 import { shallow } from "enzyme"
 import { NavBar } from "../index"
+import { withQuerySnapshot } from "gatsby-plugin-testing"
 
 describe("NavBar", () => {
-  it("renders the NavBar correctly", () => {
-    let wrapper = shallow(<NavBar></NavBar>)
-    expect(wrapper).toHaveLength(1)
-  })
+  it(
+    "renders the NavBar correctly",
+    withQuerySnapshot(() => {
+      let wrapper = shallow(<NavBar></NavBar>)
+      expect(wrapper).toHaveLength(1)
+    })
+  )
 })
