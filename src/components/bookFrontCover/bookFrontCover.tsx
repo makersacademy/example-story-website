820da0ddad8aa04c7aa5c056a9bfa92da1584497
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-21 16:42:36 +0100

    Pass image to developerImage component

--- src/components/bookFrontCover/bookFrontCover.tsx
+++ src/components/bookFrontCover/bookFrontCover.tsx
@@ -7,17 +7,19 @@ interface Props {
   greeting: string
   jobTitle: string
   careerAim: string
+  image: any
 }
 
 export const BookFrontCover = (props: Props) => {
-  const { greeting, jobTitle, careerAim } = props
+  const { greeting, jobTitle, careerAim, image } = props
+  console.log({ image })
   return (
     <div className="container">
       <div className="row align-items-center">
         <div className="col-6 offset-3 p-0">
           <div id="book" className="container py-3">
             <div className="spine ">
-              <DeveloperImage />
+              <DeveloperImage image={image} />
               <Heading greeting={greeting} jobTitle={jobTitle} />
               <SubHeading careerAim={careerAim} />
             </div>
