b7df9ba8c34cf921d56080b32f720fff026d553e
Gina Frankel <g.frankel22@gmail.com>
2020-09-21 11:58:51 +0100

    :lipstick: Add dropdown for navbar that appears on small screen
    - Use reactstrap libary

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useState } from "react"
 import "bootstrap/dist/css/bootstrap.css"
+import { Collapse, Navbar, NavbarToggler, Nav } from "reactstrap"
 
 import { navBar } from "../../labels"
 import { NavBarItem } from "../index"
@@ -9,14 +10,36 @@ export const NavBar = () => {
   const listItems = navBar.chapters.map((chapter, index) => (
     <NavBarItem key={index} chapter={chapter}></NavBarItem>
   ))
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => setIsOpen(!isOpen)
+
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark container py-5 ">
-        <div>
-          <ul className=" ml-auto navbar-nav">{listItems}</ul>
-          <div className="rectangle"></div>
-        </div>
-      </nav>
+      <Navbar light expand="md">
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <div className=" navbar-nav">{listItems}</div>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </div>
   )
 }
+// const listItems = navBar.chapters.map((chapter, index) => (
+//   <NavBarItem key={index} chapter={chapter}></NavBarItem>
+// ))
+// return (
+//   <div>
+//     <nav className="navbar navbar-expand-lg navbar-dark  py-5 ">
+//       <Button id="toggler"> Toggle</Button>
+//       <UncontrolledCollapse toggler="#toggler">
+//         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+//           <div className=" navbar-nav">{listItems}</div>
+//           {/* <div className="rectangle"></div> */}
+//         </div>
+//       </UncontrolledCollapse>
+//     </nav>
+//   </div>
+// )
