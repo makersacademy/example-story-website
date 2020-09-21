238be4d435a1addd81fdd10db4965a8b99c56148
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-21 14:45:23 +0100

    Delete comments

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -47,19 +47,3 @@ export const NavBar = () => {
     </div>
   )
 }
-// const listItems = navBar.chapters.map((chapter, index) => (
-//   <NavBarItem key={index} chapter={chapter}></NavBarItem>
-// ))
-// return (
-//   <div>
-//     <nav className="navbar navbar-expand-lg navbar-dark  py-5 ">
-//       <Button id="toggler"> Toggle</Button>
-//       <UncontrolledCollapse toggler="#toggler">
-//         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-//           <div className=" navbar-nav">{listItems}</div>
-//           {/* <div className="rectangle"></div> */}
-//         </div>
-//       </UncontrolledCollapse>
-//     </nav>
-//   </div>
-// )
