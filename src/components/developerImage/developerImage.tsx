d6f18a16a9505d93268344e12b2d743284d55330
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-21 16:42:50 +0100

    Display developer image

--- src/components/developerImage/developerImage.tsx
+++ src/components/developerImage/developerImage.tsx
@@ -1,15 +1,20 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.css"
 
-import { Image } from "../index"
+// import { Image } from "../index"
 import "../bookFrontCover/bookFrontCover.scss"
 
-export const DeveloperImage = () => {
+interface Props {
+  image: any
+}
+
+export const DeveloperImage = (props: Props) => {
+  const { image } = props
   return (
     <div>
       <div className="row">
         <div className="col-6 offset-3 pt-5">
-          <Image />
+          <img alt="myPicture" src={image.childImageSharp.fluid.src} />
         </div>
       </div>
     </div>
