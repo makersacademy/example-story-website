608c889de13febbcf387e4add047f8e60d37bef5
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-22 11:22:52 +0100

    Add getPageQueryData to enable test

--- src/components/bookFrontCover/bookFrontCover.spec.tsx
+++ src/components/bookFrontCover/bookFrontCover.spec.tsx
@@ -1,17 +1,19 @@
 import React from "react"
 import { shallow } from "enzyme"
 import { BookFrontCover } from "../index"
+import { getPageQueryData } from "gatsby-plugin-testing"
 
 describe("BookFronCover", () => {
-  it("renders a page correctly", () => {
+  it("renders a page correctly", async () => {
     let wrapper = shallow(
       <BookFrontCover
-        greeting="hello"
-        jobTitle="developer"
-        careerAim="data ops engineer"
-        image
+        greeting={await getPageQueryData("index")}
+        jobTitle={await getPageQueryData("index")}
+        careerAim={await getPageQueryData("index")}
+        image={await getPageQueryData("index")}
       />
     )
+
     expect(wrapper).toHaveLength(1)
   })
 })
