8d586c55c22b38355b26244fa0bee2d9b19b405e
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-09-22 11:45:40 +0100

    Wrap test in withQuerySnapshot to enable test in CI

--- src/components/bookFrontCover/bookFrontCover.spec.tsx
+++ src/components/bookFrontCover/bookFrontCover.spec.tsx
@@ -1,19 +1,22 @@
 import React from "react"
 import { shallow } from "enzyme"
 import { BookFrontCover } from "../index"
-import { getPageQueryData } from "gatsby-plugin-testing"
+import { getPageQueryData, withQuerySnapshot } from "gatsby-plugin-testing"
 
 describe("BookFronCover", () => {
-  it("renders a page correctly", async () => {
-    let wrapper = shallow(
-      <BookFrontCover
-        greeting={await getPageQueryData("index")}
-        jobTitle={await getPageQueryData("index")}
-        careerAim={await getPageQueryData("index")}
-        image={await getPageQueryData("index")}
-      />
-    )
+  it(
+    "renders a page correctly",
+    withQuerySnapshot(async () => {
+      let wrapper = shallow(
+        <BookFrontCover
+          greeting={await getPageQueryData("index")}
+          jobTitle={await getPageQueryData("index")}
+          careerAim={await getPageQueryData("index")}
+          image={await getPageQueryData("index")}
+        />
+      )
 
-    expect(wrapper).toHaveLength(1)
-  })
+      expect(wrapper).toHaveLength(1)
+    })
+  )
 })
