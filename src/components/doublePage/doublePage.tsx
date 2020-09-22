0681811a6179eaabd29a02062bac67f404564103
Gina Frankel <g.frankel22@gmail.com>
2020-09-22 11:53:58 +0100

    :recycle: Extract leftPage map to LeftPage component

--- src/components/doublePage/doublePage.tsx
+++ src/components/doublePage/doublePage.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.css"
 
+import { LeftPage } from "../index"
 import { BookSection } from "../index"
 import "./doublePage.scss"
 
@@ -27,15 +28,11 @@ export const DoublePage = (props: Props) => {
 
   const listLeftPage = leftPage.map((leftPageItem: any, index: number) => {
     return (
-      <div className="col-md-6 col-lg-6">
-        <div className="px-3 py-2">
-          <div className="box">
-            <h2 className="box-title px-2 my-0 py-2 "> {leftPageItem.title}</h2>
-            <h3 className="box-subtitle px-2">{leftPageItem.subTitle}</h3>
-            <p className="px-2 box-content">{leftPageItem.content}</p>
-          </div>
-        </div>
-      </div>
+      <LeftPage
+        key={index}
+        index={index}
+        leftPageItem={leftPageItem}
+      ></LeftPage>
     )
   })
 
