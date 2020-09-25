f6deadaf0edd60ecad85e403716dd716728bee7a
jamesAforster <jamesalexforster@gmail.com>
2020-09-25 10:45:13 +0100

    adds border to bookSection titles

--- src/components/bookSection/bookSection.tsx
+++ src/components/bookSection/bookSection.tsx
@@ -33,7 +33,7 @@ export const BookSection = (props: Props) => {
     <div className={`col-md-6 col-lg-6  ${style}`}>
       <div className="px-3 py-2">
         <div className="box">
-          <h2 className="box-title px-2 my-0 py-2 ">{title}</h2>
+          <h2 className="box-title px-2 my-0 py-2">{title}</h2>
           <h3 className="box-subtitle px-2">{subTitle}</h3>
           <p className="px-2 box-content">{content}</p>
           {picture}
