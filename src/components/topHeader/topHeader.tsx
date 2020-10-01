8d78abbb9141b4f32e7f2dd1a5ba55b022a66009
jamesAforster <jamesalexforster@gmail.com>
2020-10-01 17:16:49 +0100

    creates topHeader.scss and initial styling

--- src/components/topHeader/topHeader.tsx
+++ src/components/topHeader/topHeader.tsx
@@ -1,17 +1,19 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.css"
+import "./topHeader.scss"
 
 interface Props {
   greeting: string
   jobTitle: string
 }
 
-export const TopHeader = (props:Props) => {
+export const TopHeader = (props: Props) => {
   const { jobTitle, greeting } = props
 
   return (
     <div>
-      <h1>{jobTitle}{greeting}</h1>
+      <h1>{greeting}</h1>
+      <h2>{jobTitle}</h2>
     </div>
   )
 }
