ae49267d394dc31564afea18de70dbf49cc23490
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-10-05 17:18:02 +0100

    Remove default propTypes

--- src/components/seo/seo.tsx
+++ src/components/seo/seo.tsx
@@ -72,17 +72,4 @@ function SEO({ description, lang, meta, title }: Props) {
   )
 }
 
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
-}
-
 export { SEO }
