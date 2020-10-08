6aa0a70ceffd8f0e5b86cf4eb4f912e8d89df97e
jamesAforster <jamesalexforster@gmail.com>
2020-10-08 15:18:06 +0100

    Links now created dynamically on each page

--- src/components/doublePage/doublePage.tsx
+++ src/components/doublePage/doublePage.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.css"
 import { Link } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby"
 
 import { LeftPage, RightPage } from "../index"
 import "./doublePage.scss"
@@ -17,15 +18,47 @@ interface Props {
   title: string
   leftPage: Array<PageItem>
   rightPage: Array<PageItem>
+  leftPageLink: string
+  rightPageLink: string
 }
 
 export const DoublePage = (props: Props) => {
-  const { title, leftPage, rightPage } = props
+  const { title, leftPage, rightPage} = props
 
+  const listLinks = useStaticQuery(graphql`
+    {
+      allMarkdownRemark(sort: { fields: frontmatter___chapter }) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              jobTitle
+              greeting
+            }
+          }
+        }
+      }
+    }
+    `)
+
+    var linkArray: string[] = []
+
+    listLinks.allMarkdownRemark.edges.map(
+      ({ node }: { node: any }, index: number) => (
+        linkArray.push(node.fields.slug)
+      )
+    )
+
+    var rightLink = linkArray[linkArray.indexOf(location.pathname) + 1]
+    var leftLink = linkArray[linkArray.indexOf(location.pathname) - 1]
+    
   return (
     <div className="container d-flex justify-content-around" id="double-page">
-       <Link to="/learning/"><div className="triangle-right"></div></Link>
-        <Link to="/learning/"><div className="triangle-left"></div></Link>
+       <Link to={rightLink}><div className="triangle-right"></div></Link>
+        <Link to={leftLink}><div className="triangle-left"></div></Link>
       <div className="row ">
         <LeftPage leftPage={leftPage} title={title}></LeftPage>
         <RightPage rightPage={rightPage} />
