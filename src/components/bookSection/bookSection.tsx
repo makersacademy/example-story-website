97161693afaefe7a81cdfe92d744bfb5c8e7f9c0
Gina Frankel <g.frankel22@gmail.com>
2020-10-11 14:05:26 +0100

    :recycle: Change function to inline if w/ logical operators

--- src/components/bookSection/bookSection.tsx
+++ src/components/bookSection/bookSection.tsx
@@ -19,28 +19,8 @@ export const BookSection = (props: Props) => {
   const { pageItem } = props
   const { title, subTitle, content, image, backgroundImage } = pageItem
 
-  // consistency of approach rather than particular pattern
-
   function isBackgroundImagePresent() {
     return backgroundImage
-  }
-
-  function isAnyImagePresent() {
-    if (image || backgroundImage) {
-      return true
-    }
-  }
-
-  function addTextBox() {
-    if (isBackgroundImagePresent()) {
-      return "text-box col-lg-6 col-md-6"
-    }
-  }
-
-  function addTitleBox() {
-    if (!isAnyImagePresent()) {
-      return "title-box"
-    }
   }
 
   function addBackgroundImage() {
@@ -54,14 +34,18 @@ export const BookSection = (props: Props) => {
     }
   }
 
-  // one liner logic ok in react return - checking for presense ok
-  // do same for other functions
   return (
     <div className="px-3 pb-5">
       <div className="box" style={addBackgroundImage()}>
         {image && <Img fluid={image.childImageSharp.fluid} />}
-        <div className={`${addTextBox()} `}>
-          <h3 className={`title px-2 my-0 py-2 ${addTitleBox()} `}>{title}</h3>
+        <div className={`${backgroundImage && "text-box col-lg-6 col-md-6"} `}>
+          <h3
+            className={`title px-2 my-0 py-2 ${
+              (!image || !backgroundImage) && "title-box"
+            } `}
+          >
+            {title}
+          </h3>
           <h4 className="subtitle-box px-2">{subTitle}</h4>
           <p className="px-2 content-box">{content}</p>
         </div>
