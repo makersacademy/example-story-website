47455b25d64aa3257261dea69d600e42eb381e9c
Gina Frankel <g.frankel22@gmail.com>
2020-10-16 17:56:18 +0100

    Change styling for single image + split render to 2 conditionals

--- src/components/bookSection/bookSection.tsx
+++ src/components/bookSection/bookSection.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.css"
 import "./bookSection.scss"
-import Img from "gatsby-image"
 
 interface PageItem {
   title?: string
@@ -43,10 +42,6 @@ export const BookSection = (props: Props) => {
   let textAlignment
 
   const width = boxWidth && gridConversions[boxWidth]
-  // utitlity function
-  //const width = calculateWidth(box_width)
-
-  //? This could be moved to the render
 
   function addBackgroundImage() {
     if (image && content) {
@@ -59,33 +54,25 @@ export const BookSection = (props: Props) => {
     }
   }
 
-  let singleImage
   if (image && !content) {
-    {
-      singleImage = <Img fluid={image.childImageSharp.fluid} />
-    }
-  }
-
-  return (
-    <div className={`px-3 pb-5 col-${width}`}>
-      <div className="section-box" style={addBackgroundImage()}>
-        {singleImage}
-        <div
-          className={`${
-            image && !singleImage && "text-box col-lg-6 col-md-6"
-          } `}
-        >
-          <h3 className={`title px-2 my-0 py-2 ${!image && "title-box"} `}>
-            {title}
-          </h3>
-          <h4 className="subtitle-box px-2">{subTitle}</h4>
-          <p className={`px-2 content-box ${textAlignment}`}>{content}</p>
+    return (
+      <div className="picture">
+        <img src={image.childImageSharp.fluid.src} />
+      </div>
+    )
+  } else {
+    return (
+      <div className={`px-3 pb-5 col-${width}`}>
+        <div className="section-box" style={addBackgroundImage()}>
+          <div className={`${image && "text-box col-lg-6 col-md-6"} `}>
+            <h3 className={`title px-2 my-0 py-2 ${!image && "title-box"} `}>
+              {title}
+            </h3>
+            <h4 className="subtitle-box px-2">{subTitle}</h4>
+            <p className={`px-2 content-box ${textAlignment}`}>{content}</p>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
-// image with content --> backgroundImage
-
-//  background image - with no text
