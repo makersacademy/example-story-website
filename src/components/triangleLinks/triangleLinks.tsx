a28e895bab1fc60cbeb85952fd624f35887fa2a8
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-10-21 19:09:46 +0100

    Change currentLink to return '/' when undefined

--- src/components/triangleLinks/triangleLinks.tsx
+++ src/components/triangleLinks/triangleLinks.tsx
@@ -19,40 +19,43 @@ export const TriangleLinks = () => {
 
   var linkArray: string[] = []
 
-  listLinks.allMarkdownRemark.edges.map(
-    ({ node }: { node: any } ) => (
-      linkArray.push(node.fields.slug)
-    )
+  listLinks.allMarkdownRemark.edges.map(({ node }: { node: any }) =>
+    linkArray.push(node.fields.slug)
   )
 
-  const currentLink: string = typeof window !== "undefined" ? location.pathname : ""
+  const currentLink: string =
+    typeof window !== "undefined" ? location.pathname : "/"
 
   var rightLink: string =
-  typeof window !== "undefined" ? linkArray[linkArray.indexOf(currentLink) + 1] : ""
+    typeof window !== "undefined"
+      ? linkArray[linkArray.indexOf(currentLink) + 1]
+      : ""
 
   var leftLink: string =
-  typeof window !== "undefined" ? linkArray[linkArray.indexOf(currentLink) - 1] : ""
+    typeof window !== "undefined"
+      ? linkArray[linkArray.indexOf(currentLink) - 1]
+      : ""
 
   if (currentLink == "/") {
     return (
       <Link to={rightLink}>
-        <div className="triangle-right" style={{ left: "25px" }}/>
-      </Link>    
+        <div className="triangle-right" style={{ left: "25px" }} />
+      </Link>
     )
   } else if (currentLink == "/contact/") {
     return (
       <Link to={leftLink}>
-       <div className="triangle-left"/>
+        <div className="triangle-left" />
       </Link>
     )
   } else {
     return (
       <>
         <Link to={leftLink}>
-          <div className="triangle-left"/>
+          <div className="triangle-left" />
         </Link>
         <Link to={rightLink}>
-          <div className="triangle-right"/>
+          <div className="triangle-right" />
         </Link>
       </>
     )
