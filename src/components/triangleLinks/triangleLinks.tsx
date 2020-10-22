853991a077e8a4a19b7cc1fcf437270cdd6aa792
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-10-22 10:58:01 +0100

    Refactor to use pathName prop instead of currentLink

--- src/components/triangleLinks/triangleLinks.tsx
+++ src/components/triangleLinks/triangleLinks.tsx
@@ -2,7 +2,12 @@ import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import "../navBar/navBar.scss"
 
-export const TriangleLinks = () => {
+interface Props {
+  pathName: string
+}
+
+export const TriangleLinks = (props: Props) => {
+  const { pathName } = props
   const listLinks = useStaticQuery(graphql`
     {
       allMarkdownRemark(sort: { fields: frontmatter___chapter }) {
@@ -23,26 +28,23 @@ export const TriangleLinks = () => {
     linkArray.push(node.fields.slug)
   )
 
-  const currentLink: string =
-    typeof window !== "undefined" ? location.pathname : ""
-
   var rightLink: string =
     typeof window !== "undefined"
-      ? linkArray[linkArray.indexOf(currentLink) + 1]
+      ? linkArray[linkArray.indexOf(pathName) + 1]
       : ""
 
   var leftLink: string =
     typeof window !== "undefined"
-      ? linkArray[linkArray.indexOf(currentLink) - 1]
+      ? linkArray[linkArray.indexOf(pathName) - 1]
       : ""
 
-  if (currentLink == "/") {
+  if (pathName == "/") {
     return (
       <Link to={rightLink}>
         <div className="triangle-right" style={{ left: "25px" }} />
       </Link>
     )
-  } else if (currentLink == "/contact/") {
+  } else if (pathName == "/contact/") {
     return (
       <Link to={leftLink}>
         <div className="triangle-left" />
