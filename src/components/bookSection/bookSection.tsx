ef52061db2c42713a9b118cde0a230fe3b9e34c3
Gina Frankel <g.frankel22@gmail.com>
2020-10-22 15:33:42 +0100

    Merge branch 'furtherFormat'
    - This branch moves the boxWidthLookup table/logic from the bookSection component into a helper file
    - Also introduces a default size of 12 if boxWidth is undefined.

--- src/components/bookSection/bookSection.tsx
+++ src/components/bookSection/bookSection.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import "bootstrap/dist/css/bootstrap.css"
 import "./bookSection.scss"
 import Img from "gatsby-image"
+import { boxWidthConvert } from "./helper/colWidthHelper"
 
 interface PageItem {
   title?: string
@@ -9,6 +10,7 @@ interface PageItem {
   content?: string
   image?: any
   backgroundImage?: any
+  boxWidth?: string
 }
 
 interface Props {
@@ -17,7 +19,15 @@ interface Props {
 
 export const BookSection = (props: Props) => {
   const { pageItem } = props
-  const { title, subTitle, content, image, backgroundImage } = pageItem
+
+  const {
+    title,
+    subTitle,
+    content,
+    image,
+    boxWidth,
+    backgroundImage,
+  } = pageItem
 
   function isBackgroundImagePresent() {
     return backgroundImage
@@ -57,14 +67,18 @@ export const BookSection = (props: Props) => {
     picture = <Img fluid={image.childImageSharp.fluid} />
   }
 
+  const columnWidth = boxWidthConvert(boxWidth!)
+
+  const alignText = columnWidth ? "text-center" : ""
+
   return (
-    <div className="px-3 pb-5">
+    <div className={`px-3 pb-5 col-${columnWidth}`}>
       <div className="box" style={addBackgroundImage()}>
         {picture}
         <div className={`${addTextBox()} `}>
           <h3 className={`title px-2 my-0 py-2 ${addTitleBox()} `}>{title}</h3>
           <h4 className="subtitle-box px-2">{subTitle}</h4>
-          <p className="px-2 content-box">{content}</p>
+          <p className={`px-2 content-box ${alignText}`}>{content}</p>
         </div>
       </div>
     </div>
