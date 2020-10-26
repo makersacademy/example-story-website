834a9e208bc61020987516853c5a2e2eb13de19b
jamesAforster <jamesalexforster@gmail.com>
2020-10-26 16:05:45 +0000

    next page link for homepage now in correct position

--- src/components/nextPageLink/nextPageLink.tsx
+++ src/components/nextPageLink/nextPageLink.tsx
@@ -4,14 +4,26 @@ import "../navBar/navBar.scss"
 
 interface Props {
   nextLink: string
+  homePage: boolean
 }
 
 export const NextPageLink = (props: Props) => {
-  const { nextLink } = props
-  return (
+  const { nextLink, homePage } = props
+    let home
+    if (homePage == true) {
+      home = {left: '2px'}
+    
+    return (
     <Link to={nextLink}>
-      <div className="triangle-right" />
-    </Link>
-  )
+      <div className="triangle-right-home"/>
+    </Link>)
+    }
+    else {
+      return (
+        <Link to={nextLink}>
+          <div className="triangle-right"/>
+        </Link>
+      )
+    }
 }
 
