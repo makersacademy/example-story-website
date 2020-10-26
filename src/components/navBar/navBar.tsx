d9cdfa8d1afb8d5f3be8df74ff00adb6820655ad
Gina Frankel <g.frankel22@gmail.com>
2020-10-26 12:17:44 +0000

    :recycle: Split layoutData prop into frontmatter + fields

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -6,24 +6,37 @@ import { Collapse, Navbar, NavbarToggler, Nav } from "reactstrap"
 import { NavBarItem, TopHeader, TriangleLinks } from "../index"
 import "./navBar.scss"
 
+interface FrontmatterData {
+  title: string
+  jobTitle: string
+  greeting: string
+}
 interface Props {
   pathName: string
   layoutData: any
+  listLinks: Array<string>
+  frontmatterData: Array<FrontmatterData>
 }
 
 export const NavBar = (props: Props) => {
-  const { pathName, layoutData } = props
-  console.log(layoutData)
+  const { pathName, layoutData, listLinks, frontmatterData } = props
+  console.log(frontmatterData)
 
-  var listItems = layoutData.allMarkdownRemark.edges.map(
-    ({ node }: { node: any }, index: number) => (
-      <NavBarItem
-        key={index}
-        chapterTitle={node.frontmatter.title}
-        chapterLink={node.fields.slug}
-      ></NavBarItem>
-    )
-  )
+  var listItems = listLinks.map((link: any, index: number) => (
+    <NavBarItem
+      key={index}
+      chapterTitle={frontmatterData[index].title}
+      chapterLink={link}
+    ></NavBarItem>
+  ))
+
+  // problem:
+  // - 2 different arrays and we need to map both of them
+  // map one array then map the second one
+
+  // if we have the slugs we can make the title
+  // id
+
   const [isOpen, setIsOpen] = useState(false)
   const toggle = () => setIsOpen(!isOpen)
 
