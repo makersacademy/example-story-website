261c6f093d44f4dbc21edf21a7a8cc130c3ab362
Gina Frankel <g.frankel22@gmail.com>
2020-10-28 11:46:33 +0000

    :white_check_mark: Fix failing tests
    - Update props
    - Update child components

--- src/components/layout/index.spec.tsx
+++ src/components/layout/index.spec.tsx
@@ -2,17 +2,51 @@ import React from "react"
 import ReactNode from "react"
 import { shallow } from "enzyme"
 import { Layout } from "./index"
-import { NavBar } from "../index"
+import { NavigationContainer } from "../index"
 
 describe("Layout", () => {
   it("renders a page correctly", () => {
-    let wrapper = shallow(<Layout pathName="" children=""></Layout>)
+    const listLinks = ["\\"]
+    const frontMatterData = [
+      {
+        title: "Home",
+        jobTitle: "Junior Developer",
+        greeting: "Hi I'm Joseph Jones",
+      },
+    ]
+
+    const wrapper = shallow(
+      <Layout
+        pathName=".\"
+        children=""
+        listLinks={listLinks}
+        frontmatterData={frontMatterData}
+      ></Layout>
+    )
     expect(wrapper).toHaveLength(1)
   })
 
-  it("renders the NavBar correctly", () => {
-    let wrapper = shallow(<Layout pathName="" children=""></Layout>)
-    let navBar = <NavBar pathName="" layoutData=""></NavBar>
-    expect(wrapper.containsMatchingElement(navBar)).toEqual(true)
+  it("renders the NavigationContainer correctly", () => {
+    const listLinks = ["\\"]
+    const frontMatterData = [
+      {
+        title: "Home",
+        jobTitle: "Junior Developer",
+        greeting: "Hi I'm Joseph Jones",
+      },
+    ]
+    const wrapper = shallow(
+      <Layout
+        pathName="\"
+        children=""
+        listLinks={listLinks}
+        frontmatterData={frontMatterData}
+      ></Layout>
+    )
+    const navigationContainer = (
+      <NavigationContainer pathName="\"></NavigationContainer>
+    )
+
+    expect(wrapper.containsMatchingElement(navigationContainer)).toEqual(true)
   })
 })
