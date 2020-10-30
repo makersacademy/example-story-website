a6f56492df997554e6016cce2ab4f97c38364577
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-10-30 20:12:38 +0000

    Remove defunct props and typings missed during refactor

--- src/components/layout/index.spec.tsx
+++ src/components/layout/index.spec.tsx
@@ -1,50 +1,17 @@
 import React from "react"
-import ReactNode from "react"
+
 import { shallow } from "enzyme"
 import { Layout } from "./index"
 import { NavigationContainer } from "../index"
 
 describe("Layout", () => {
   it("renders a page correctly", () => {
-    const listLinks = ["\\"]
-    const frontMatterData = [
-      {
-        title: "Home",
-        jobTitle: "Junior Developer",
-        greeting: "Hi I'm Joseph Jones",
-      },
-    ]
-
-    const wrapper = shallow(
-      <Layout
-        slug="\"
-        children=""
-        listLinks={listLinks}
-        frontmatterData={frontMatterData}
-        chapter="0"
-      ></Layout>
-    )
+    const wrapper = shallow(<Layout slug="\" children="" chapter="0"></Layout>)
     expect(wrapper).toHaveLength(1)
   })
 
   it("renders the NavigationContainer correctly", () => {
-    const listLinks = ["x"]
-    const frontMatterData = [
-      {
-        title: "Home",
-        jobTitle: "Junior Developer",
-        greeting: "Hi I'm Joseph Jones",
-      },
-    ]
-    const wrapper = shallow(
-      <Layout
-        slug="x"
-        children=""
-        listLinks={listLinks}
-        frontmatterData={frontMatterData}
-        chapter="0"
-      ></Layout>
-    )
+    const wrapper = shallow(<Layout slug="x" children="" chapter="0"></Layout>)
     const navigationContainer = (
       <NavigationContainer chapter="0" slug="x"></NavigationContainer>
     )
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,18 +3,10 @@ import React, { ReactNode } from "react"
 import { NavigationContainer } from "../index"
 import "./index.scss"
 
-interface FrontmatterData {
-  title: string
-  jobTitle: string
-  greeting: string
-}
-
 interface Props {
   children: ReactNode
   slug: string
   chapter: string
-  listLinks: Array<string>
-  frontmatterData: Array<FrontmatterData>
 }
 
 const Layout = (props: Props) => {
