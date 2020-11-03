8b151e2787f96b4bafe7f72d3d895f5875527056
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-11-03 15:25:23 +0000

    Add responsive bookcase img to custom 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,18 @@
 import React from "react"
-
 import { SEO } from "../components"
 
 const NotFoundPage = () => (
   <>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="d-flex align-items-center flex-column">
+      <h1>NOT FOUND</h1>
+      <img
+        src="https://www.ikea.com/gb/en/images/products/billy-bookcase-white__0625599_PE692385_S5.JPG?f=xl "
+        style={{ maxWidth: "40%" }}
+        alt="bookcase_404"
+      />
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
   </>
 )
 
