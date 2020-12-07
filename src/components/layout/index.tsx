c85104339036ea0637d3ef60599563903c48cd44
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-12-07 14:27:17 +0000

    Add graphql query to generate next and previous navigation links

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react"
-
+import { graphql, useStaticQuery } from "gatsby"
 import { NavigationContainer } from "../index"
 import "./index.scss"
 
@@ -11,6 +11,35 @@ interface Props {
 
 const Layout = (props: Props) => {
   const { children, slug, chapter } = props
+
+  const nextPreviousData = useStaticQuery(graphql`
+    {
+      allMarkdownRemark(sort: { fields: frontmatter___chapter }) {
+        edges {
+          previous {
+            fields {
+              slug
+            }
+          }
+          next {
+            fields {
+              slug
+            }
+          }
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              jobTitle
+              greeting
+            }
+          }
+        }
+      }
+    }
+  `)
 
   return (
     <div>
